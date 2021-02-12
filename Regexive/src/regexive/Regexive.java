273d9d8eb8a4e75f09c6c052ba55bf8e7f01e56d
joseCastro123 <josesincu@gmail.com>
2021-02-11 23:34:49 -0600

    Creando archivos jflex and jcup, interfaz de usuario

---
+++ Regexive/src/regexive/Regexive.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regexive;
+
+/**
+ *
+ * @author dark
+ */
+public class Regexive {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Grafica graf= null;
+        graf = new Grafica();
+        graf.show();
+        System.out.println("Hola mundo");
+    }
+    
+}
